e7d69e243d5d2f4d08bcd2b2d2d8996385aed72b
WolfyzDBois <thibault.ponsot@epita.fr>
2025-05-20 11:57:08 +0200

    🚀 Déploiement site à 2025-05-20 11:57:08

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  base: '/syndromefear/', // très important pour le bon routage
+  base: '/syndromefear/',
   site: 'https://wolfyzdbois-student.github.io/syndromefear_site/',
   integrations: [tailwind()],
   outDir: './dist'
--- dist/js/galery.js
+++
@@ -1,7 +0,0 @@
-const images = document.querySelectorAll(".gallery-image");
-      let current = 0;
-      setInterval(() => {
-        images[current].classList.add("hidden");
-        current = (current + 1) % images.length;
-        images[current].classList.remove("hidden");
-      }, 10000);
