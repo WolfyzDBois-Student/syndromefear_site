58dc26a1615c964bb8f411610af3ea89ef15fa79
WolfyzDBois <thibault.ponsot@epita.fr>
2025-05-20 15:41:05 +0200

    🚀 Déploiement site à 2025-05-20 15:41:05

--- dist/js/nav-toggle.js
+++ dist/js/nav-toggle.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const toggleButton = document.querySelector(".nav-toggle");
-    const collapsedMenu = document.querySelector(".navbar-collapsed");
-  
-    if (toggleButton && collapsedMenu) {
-      toggleButton.addEventListener("click", () => {
-        collapsedMenu.classList.toggle("open");
+  const toggleBtn = document.querySelector(".nav-toggle");
+  const collapsedMenu = document.querySelector(".navbar-collapsed");
+  const links = collapsedMenu?.querySelectorAll("a");
+
+  if (toggleBtn && collapsedMenu) {
+    toggleBtn.addEventListener("click", () => {
+      collapsedMenu.classList.toggle("open");
+    });
+
+    links?.forEach(link => {
+      link.addEventListener("click", () => {
+        collapsedMenu.classList.remove("open");
       });
-    }
-  });
-  
+    });
+  }
+});
