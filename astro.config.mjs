dd2f476eff9611c67ee7d5179bd8c8b7a0b704f8
WolfyzDBois <thibault.ponsot@epita.fr>
2025-05-20 12:26:41 +0200

    🚀 Déploiement site à 2025-05-20 12:26:41

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  base: '/syndromefear/',
+  /* base: '/syndromefear/', */
   site: 'https://wolfyzdbois-student.github.io/syndromefear_site/',
   integrations: [tailwind()],
   outDir: './dist'
